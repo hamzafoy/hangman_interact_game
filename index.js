7a19289bc692c01681352ec37f14cc61554de084
Ross Foy <hamzaoflouisville@gmail.com>
2021-12-23 19:05:54 -0500

    first commit, beginning to build out and notate development of game state object.

---
+++ index.js
@@ -0,0 +1,37 @@
+/*:::::::PLANNING THE LOGIC OF THE HANDMAN GAME::::::
+
+Store the game loop in an object. This object should have an overall
+variable storing a boolean indicating whether the game is in session
+or not and should have a setter method that sets the word that is being
+guessed during an active game.
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+
+class gameInstance {
+
+    constructor() {
+        this.isActive = false;
+        this.currentWordInPlay = null;
+    }
+
+    get currentWord() {
+        console.log(this.currentWordInPlay);
+        switch(typeof this.currentWordInPlay == 'string') {
+            case true:
+            return `The current word in play is ${this.currentWordInPlay}`;
+            case false:
+            return `No word is in play currently!`
+        }
+        
+    }
+
+    set determineWordInPlay(input) {
+        this.currentWordInPlay = input;
+    }
+
+};
+
+let newGame = new gameInstance();
+console.log(newGame.currentWord);
+newGame.determineWordInPlay = 'Smorgasbord';
+console.log(newGame.currentWord);
